68585652314252349fc8f88a785b98041450353a
Kyle Davis <kyle.davis@ipvs.uni-stuttgart.de>
2021-08-04 10:16:34 +0200

    Update RS-LS

--- src/acceleration/MVQNAcceleration.cpp
+++ src/acceleration/MVQNAcceleration.cpp
@@ -58,7 +58,7 @@ MVQNAcceleration::MVQNAcceleration(
       _imvjRestart(false),
       _chunkSize(chunkSize),
       _RSLSreusedTimesteps(RSLSreusedTimesteps),
-      _usedColumnsPerTstep(5),
+      _usedColumnsPerTstep(20),
       _nbRestarts(0),
       //_info2(),
       _avgRank(0)
